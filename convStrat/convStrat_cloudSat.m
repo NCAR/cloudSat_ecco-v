2dd3cb41264dd81522b9dde8dcdd657a3e9fa183
ulrom <romatsch@ucar.edu>
2021-12-10 14:53:34 -0700

    Working on paper figure

--- convStrat/convStrat_cloudSat.m
+++ convStrat/convStrat_cloudSat.m
@@ -20,7 +20,7 @@ divAlt=6; % Estimated altitude of divergence level in km
 dataDir='/scr/snow2/rsfdata/projects/cset/cloudSat/hdf/';
 infile='2015184230321_48842_CS_2B-GEOPROF_GRANULE_P1_R05_E06_F00.hdf';
 
-figdir=['/scr/sci/romatsch/other/convStratCloudSat/'];
+figdir=['/scr/sci/romatsch/other/convStratPaperHCR/'];
 
 %% Read data
 
@@ -152,7 +152,24 @@ col1D=colmapSC(stratConv1D,:);
 
 close all
 
-f1 = figure('Position',[200 500 1500 1100],'DefaultAxesFontSize',12,'visible',showPlot);
+textAlt=11.3;
+textDate=datetime(2015,7,4,0,5,32);
+
+close all
+
+wi=9;
+hi=6;
+
+fig1=figure('DefaultAxesFontSize',11,'DefaultFigurePaperType','<custom>','units','inch','position',[3,100,wi,hi]);
+fig1.PaperPositionMode = 'manual';
+fig1.PaperUnits = 'inches';
+fig1.Units = 'inches';
+fig1.PaperPosition = [0, 0, wi, hi];
+fig1.PaperSize = [wi, hi];
+fig1.Resize = 'off';
+fig1.InvertHardcopy = 'off';
+
+set(fig1,'color','w');
 
 s1=subplot(4,1,1);
 
@@ -165,12 +182,11 @@ ylabel('Altitude (km)');
 caxis([-35 25]);
 ylim([0 ylimUpper]);
 xlim([data.time(1),data.time(end)]);
-colorbar
+set(gca,'XTickLabel',[]);
+cb1=colorbar;
 grid on
 box on
-title('Reflectivity (dBZ)')
-s1pos=s1.Position;
-s1.Position=[s1pos(1),s1pos(2),s1pos(3),s1pos(4)];
+text(textDate,textAlt,'(a) Reflectivity (dBZ)','FontSize',11,'FontWeight','bold');
 
 s2=subplot(4,1,2);
 
@@ -181,31 +197,11 @@ ylabel('Altitude (km)');
 caxis([0 1]);
 ylim([0 ylimUpper]);
 xlim([data.time(1),data.time(end)]);
-colorbar
+cb2=colorbar;
+set(gca,'XTickLabel',[]);
 grid on
 box on
-title('Convectivity')
-s2pos=s2.Position;
-s2.Position=[s2pos(1),s2pos(2),s1pos(3),s2pos(4)];
-
-s3=subplot(4,1,3);
-
-hold on
-surf(data.time,data.asl./1000,classBasic,'edgecolor','none');
-view(2);
-ylabel('Altitude (km)');
-caxis([0.5 3.5]);
-colormap(s3,[0,0,1;0.32,0.78,0.59;1 0 0]);
-cb=colorbar;
-cb.Ticks=[1,2,3];
-cb.TickLabels=cat(2,{'Stratiform','Mixed','Convective'});
-ylim([0 ylimUpper]);
-xlim([data.time(1),data.time(end)]);
-title('Basic stratiform/convective partitioning')
-grid on
-box on
-s3pos=s3.Position;
-s3.Position=[s3pos(1),s3pos(2),s1pos(3),s3pos(4)];
+text(textDate,textAlt,'(b) Convectivity','FontSize',11,'FontWeight','bold');
 
 s5=subplot(30,1,30);
 
@@ -217,10 +213,8 @@ s5.Colormap=colmapSC;
 xlim([data.time(1),data.time(end)]);
 grid on
 box on
-s5pos=s5.Position;
-s5.Position=[s5pos(1),s5pos(2)-0.023,s1pos(3),s5pos(4)];
 
-s4=subplot(4,1,4);
+s4=subplot(4,1,3);
 
 hold on
 surf(data.time,data.asl./1000,classSubPlot,'edgecolor','none');
@@ -231,18 +225,23 @@ ylim([0 ylimUpper]);
 xlim([data.time(1),data.time(end)]);
 s4.Colormap=colmapSC;
 caxis([0.5 9.5]);
-cb=colorbar;
-cb.Ticks=1:9;
-cb.TickLabels={'Strat Low','Strat Mid','Strat High','Mixed',...
+cb4=colorbar;
+cb4.Ticks=1:9;
+cb4.TickLabels={'Strat Low','Strat Mid','Strat High','Mixed',...
     'Conv','Conv Elev','Conv Shallow','Conv Mid','Conv Deep'};
 set(gca,'XTickLabel',[]);
 grid on
 box on
-title('Stratiform/convective partitioning')
-s4pos=s4.Position;
-s4.Position=[s4pos(1),s4pos(2),s1pos(3),s4pos(4)];
+text(textDate,textAlt,'(c) Convective/stratiform classification','FontSize',11,'FontWeight','bold');
 
-linkaxes([s1 s2 s3 s4],'xy');
+s1.Position=[0.049 0.7 0.82 0.29];
+s2.Position=[0.049 0.4 0.82 0.29];
+s4.Position=[0.049 0.1 0.82 0.29];
+s5.Position=[0.049 0.065 0.82 0.02];
+
+cb1.Position=[0.875,0.7,0.02,0.29];
+cb2.Position=[0.875,0.4,0.02,0.29];
+cb4.Position=[0.875,0.1,0.02,0.29];
 
 set(gcf,'PaperPositionMode','auto')
-print(f1,[figdir,'convStrat_',datestr(data.time(1),'yyyymmdd_HHMMSS'),'_to_',datestr(data.time(end),'yyyymmdd_HHMMSS')],'-dpng','-r0')
+print(fig1,[figdir,'cloudSat.png'],'-dpng','-r0')
